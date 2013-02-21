9390a8f2cc12ef8341c4f7e6b9d29d2cbb4a3e63
Alaric <alaric@alaric.(none)>
2013-02-21 03:48:45 -0500

    Uploading basic ffserver/ffmpeg code

---
+++ video_streaming/basic_server/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install git-core cmake libglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev
+git clone git://github.com/OpenKinect/libfreenect.git
+cd libfreenect
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig /usr/local/lib64/
+sudo glview
