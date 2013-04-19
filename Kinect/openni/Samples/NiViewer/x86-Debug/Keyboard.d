03e13fc36eed6fdc3db0aeb0691728353fe78147
sambadog17@gmail.com <sambadog17@gmail.com>
2013-04-18 21:58:31 -0400

    adding the kinect files!

---
+++ Kinect/openni/Samples/NiViewer/x86-Debug/Keyboard.d
@@ -0,0 +1,28 @@
+x86-Debug/Keyboard.d x86-Debug/Keyboard.o: Keyboard.cpp Keyboard.h \
+ /usr/include/string.h /usr/include/features.h \
+ /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h /usr/include/xlocale.h
+
+Keyboard.h:
+
+/usr/include/string.h:
+
+/usr/include/features.h:
+
+/usr/include/i386-linux-gnu/bits/predefs.h:
+
+/usr/include/i386-linux-gnu/sys/cdefs.h:
+
+/usr/include/i386-linux-gnu/bits/wordsize.h:
+
+/usr/include/i386-linux-gnu/gnu/stubs.h:
+
+/usr/include/i386-linux-gnu/gnu/stubs-32.h:
+
+/usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h:
+
+/usr/include/xlocale.h:
