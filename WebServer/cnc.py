638c822f1033b4c0b7d585e950226bf73e16dc01
jroose <jroose@gmail.com>
2013-04-22 04:08:06 -0400

    Added metrics to web interface

--- WebServer/cnc.py
+++ WebServer/cnc.py
@@ -11,10 +11,6 @@ class WebsocketProtocol(protocol.Protocol):
 		self.last_keys = None
 		self.connected = 0
 
-	def sendCmd(self, robot, command, data1, data2):
-		print "Writing command to robot",robot.name,hex(command), hex(data1), hex(data2)
-		robot.write(bytes("".join([chr(command % 256),chr(data1 % 256),chr(data2 % 256)])))
-
 	def dataReceived(self, jdata):
 		if not self.connected:
 			print 'Client Connected at',self.transport.getHandle().getpeername()
@@ -30,11 +26,14 @@ class WebsocketProtocol(protocol.Protocol):
 			return
 
 		if data['robotid'] not in Minotaur.instances.keys():
-			print data['robotid'],"not a valid robot"
-			print "Valid robots are:", "".join(Minotaur.instances.keys())
+			if len(Minotaur.instances):
+				print data['robotid'],"not a valid robot"
+				print "Valid robots are:", "".join(Minotaur.instances.keys())
 			return
 		else:
 			robot = Minotaur.instances[data['robotid']]
+
+		robot.requestMetrics(self)
 
 		if 'gKeys' not in data.keys() or data['gKeys'] == None:
 			data['gKeys'] = 0
@@ -73,25 +72,25 @@ class WebsocketProtocol(protocol.Protocol):
 
 			left = abs(motors[0])|dirs[0]
 			right = abs(motors[1])|dirs[1]
-			self.sendCmd(robot, 0x01, left, right)
+			robot.sendCmd( 0x01, left, right)
 
 		if self.last_keys == 0:
 			if data['gKeys'] == 32:
-				self.sendCmd(robot, 0x10, 0x00, 0x00)
+				robot.sendCmd(0x10, 0x00, 0x00)
 			elif data['gKeys'] == 64:
-				self.sendCmd(robot, 0x20, 0x01, 0x00)
+				robot.sendCmd(0x20, 0x01, 0x00)
 			elif data['gKeys'] == 128:
-				self.sendCmd(robot, 0x20, 0x02, 0x00)
+				robot.sendCmd(0x20, 0x02, 0x00)
 			elif data['gKeys'] == 256:
-				self.sendCmd(robot, 0x20, 0x04, 0x00)
+				robot.sendCmd(0x20, 0x04, 0x00)
 			elif data['gKeys'] == 512:
-				self.sendCmd(robot, 0x20, 0x07, 0x00)
+				robot.sendCmd(0x20, 0x07, 0x00)
 			elif data['gKeys'] == 1024:
-				self.sendCmd(robot, 0x21, 0x00, 0x00)
+				robot.sendCmd(0x21, 0x00, 0x00)
 			elif data['gKeys'] == 2048:
-				self.sendCmd(robot, 0x22, 0x00, 0x00)
+				robot.sendCmd(0x22, 0x00, 0x00)
 			elif data['gKeys'] == 4096:
-				self.sendCmd(robot, 0x23, 0x00, 0x00)
+				robot.sendCmd(0x23, 0x00, 0x00)
 
 		self.last_keys = data['gKeys']
 
@@ -102,7 +101,7 @@ class Minotaur:
 		self.name = None
 		self.loggedin = False
 		self.socket = socket
-
+		self.requests = {}
 
 	def login(self,name):
 		self.name = name
@@ -111,6 +110,16 @@ class Minotaur:
 
 	def write(self, data):
 		self.socket.transport.write(data)
+
+	def sendCmd(self, command, data1, data2):
+		print "Writing command to robot",self.name,hex(command), hex(data1), hex(data2)
+		self.write(bytes("".join([chr(command % 256),chr(data1 % 256),chr(data2 % 256)])))
+	
+	def requestMetrics(self, client):
+		if 0x30 not in self.requests.keys(): self.requests[0x30] = []
+		self.requests[0x30].append(client)
+		if len(self.requests[0x30]) == 1:
+			self.sendCmd(0x30, 0x00, 0x00)
 
 class MinotaurProtocol(protocol.Protocol):
 	def __init__(self):
@@ -124,6 +133,26 @@ class MinotaurProtocol(protocol.Protocol):
 		if(not self.minotaur.loggedin):
 			self.minotaur.login(data)
 			print "Minotaur Connected with id:", self.minotaur.name,'at',self.transport.getHandle().getpeername()
+		else:
+			command = struct.unpack("<B",data)[0]
+			if command == 0x30:
+				if command in self.minotaur.requests.keys():
+					metrics_tuple = struct.unpack("<BIfffHHBB",data)
+					metrics = (
+							'command',
+							'timestamp',
+							'x','y',
+							'orient',
+							'left_encoder','right_encoder',
+							'ir_bank',
+							'battery'
+						)
+					metrics = dict(zip(metrics,metrics_tuple))
+					for client in self.minotaur.requests[command]:
+						client.transport.write(json.dumps(metrics))
+
+					self.minotaur.requests[command] = []
+				
 
 minotaur_factory = protocol.ServerFactory()
 minotaur_factory.protocol = MinotaurProtocol
