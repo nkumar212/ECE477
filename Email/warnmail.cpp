90c3a5691b8ba9c5b4f5f05de3193f068db40f34
jroose <jroose@gmail.com>
2013-04-23 22:43:25 -0400

    Got the warnmail

---
+++ Email/warnmail.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "warnmail.h"
+
+void warn(char* toAddr);
+void warn(char* toAddr, char* filename);
+
+void warn(char* toAddr){
+
+	// First, get the local time and date of the incident
+	time_t rawtime;
+	struct tm * timeinfo;
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	// Initialize and populate a buffer containing the message to be sent
+	char msgBuffer[110];
+	sprintf(msgBuffer, "An intruder was detected by the minotaur at the following local time and date: %s",asctime(timeinfo));
+
+	// Initialize and populate a buffer containing the sendEmail command and its flags
+	char cmdBuffer[400];
+	sprintf(cmdBuffer,"sendEmail -f 477Minotaur@gmail.com -t %s -u \"Minotaur Security\" -m \"%s\" -s smtp.gmail.com -o tls=yes -xu 477Minotaur@gmail.com -xp ZAQ\!XSW@ -q",toAddr,msgBuffer);
+
+	// Execute the command
+	system(cmdBuffer);
+}
+
+void warn(char* toAddr, char* filename){
+
+	// First, get the local time and date of the incident
+	time_t rawtime;
+	struct tm * timeinfo;
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	// Initialize and populate a buffer containing the message to be sent
+	char msgBuffer[160];
+	sprintf(msgBuffer, "An intruder was detected by the minotaur at the following local time and date: %s. See attached file for picture.",asctime(timeinfo));
+
+	// Initialize and populate a buffer containing the sendEmail command and its flags
+	char cmdBuffer[500];
+	sprintf(cmdBuffer,"sendEmail -f 477Minotaur@gmail.com -t %s -u \"Minotaur Security\" -m \"%s\" -s smtp.gmail.com -o tls=yes -xu 477Minotaur@gmail.com -xp ZAQ\!XSW@ -q -a %s",toAddr,msgBuffer,filename);
+
+	// Execute the command
+	system(cmdBuffer);
+}
---
+++ Email/warnmail.h
@@ -0,0 +1,6 @@
+#ifndef WARNMAIL_H_INCLUDED
+#define WARNMAIL_H_INCLUDED
+
+void warn(char* toAddr);
+void warn(char* toAddr, char* filename);
+#endif
